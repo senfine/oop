5091d50a60dcdc3cb99392ad5a0e12db07fffe69
Alexey Malov <alexey.malov@cpslabs.net>
2016-10-05 09:25:47 +0300

    Добавлена проверка количества аргументов коммандной строки

--- lab1/findtext/findtext/findtext.cpp
+++ lab1/findtext/findtext/findtext.cpp
@@ -3,9 +3,18 @@
 
 #include "stdafx.h"
 
+#include <iostream>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		cout << "Wrong command line\n";
+		cout << "Usage: findtext.exe <file name> <text to search>\n";
+		return 1;
+	}
     return 0;
 }
 
